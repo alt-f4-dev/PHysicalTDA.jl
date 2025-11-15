bbef28875fe6536aeb72f2e586ed5ad676505e81
alt-f4-dev <isaac.ownby@gmail.com>
2025-11-15 13:52:09 -0500

    update exports at top-level scope

--- src/PHysicalTDA.jl
+++ src/PHysicalTDA.jl
@@ -3,21 +3,23 @@ module PHysicalTDA
 #Required Dependencies
 using Sunny, LinearAlgebra, StaticArrays, Ripserer, PersistenceDiagrams, Statistics, Base.Threads
 
-#Public API
-export La2CuO4, LSWT, convert4D, collapse, 
-       MAD, pd_array_intensity, pd_sunny_intensities, 
-       persistence_entropy, betti_curvature
+#Crystals
+export La2CuO4, TMMC 
+
+#LSWT Tools 
+export LSWT, convert4D, collapse
+
+#Persistence Homology
+export MAD, pd_array_intensity, pd_sunny_intensities, persistence_entropy, betti_curvature
 
 #Internal Submodules 
 include("CrystalDB.jl"); include("LSWTtools.jl"); include("TDAPH.jl")
 
 #Lazy Plotting
-export enable_visuals, 
-       plot_persistence_diagram, plot_lifetime_diagram,
-       plot_persistence_entropy, plot_betti_curvature, 
-       plot_betti_surface, plot_dataset_overview
-const _viz_loaded = Ref(false)
+export enable_visuals, plot_persistence_diagram, plot_lifetime_diagram, plot_persistence_entropy, plot_betti_curvature, plot_betti_surface, plot_dataset_overview
 
+
+const _viz_loaded = Ref(false)
 function enable_visuals(; backend::Symbol = :gl)
     _viz_loaded[] && return nothing
 
@@ -34,68 +36,46 @@ function enable_visuals(; backend::Symbol = :gl)
 
     if backend === :gl
         @info "Activating GLMakie backend..."
-        #@eval begin
-            #import GLMakie
-            #GLMakie.activate!()
-            #mod = Base.require(Base, :GLMakie)
-            #getfield(mod, :activate!)()
-        #end
-        #Core.eval(@__MODULE__, :(import GLMakie))
-        #GLMakie.activate!()
         mod = _lazy_import(:GLMakie)
         mod !== nothing && getfield(mod, :activate!)()
     elseif backend === :cairo
         @info "Activating CairoMakie backend..."
-        #@eval begin
-            #import CairoMakie
-            #CairoMakie.activate!()
-            #mod = Base.require(Base, :CairoMakie)
-            #getfield(mod, :activate!)()
-        #end
-        #Core.eval(@__MODULE__, :(import CairoMakie))
-        #CairoMakie.activate!()
         mod = _lazy_import(:CairoMakie)
         mod !== nothing && getfield(mod, :activate!)()
     else
         error("Unknown backend: $backend (use :gl or :cairo)")
     end 
-    #@eval include(joinpath(@__DIR__, "TopoViz.jl"))
     Base.invokelatest(include, joinpath(@__DIR__,"TopoViz.jl"))
     _viz_loaded[] = true
     return nothing
 end
 
+#Confirm plotting utilities are enabled 
 @inline _ensure_viz(; backend::Symbol=:gl) = (_viz_loaded[] || enable_visuals(backend=backend))
 
-#Lazy Plot Entry
+#Lazy Plot Entry Point
 function plot_persistence_diagram(args...; backend::Symbol=:gl, kwargs...)
     _ensure_viz(backend=backend)
-    #return TopoViz.plot_persistence_diagram(args...; kwargs...)
     return Base.invokelatest(TopoViz.plot_persistence_diagram, args...; kwargs...)
 end
 function plot_lifetime_diagram(args...; backend::Symbol=:gl, kwargs...)
     _ensure_viz(backend=backend)
-    #return TopoViz.lifetime_diagram(args...; kwargs...)
     return Base.invokelatest(TopoViz.plot_lifetime_diagram, args...; kwargs...)
 end
 function plot_persistence_entropy(args...; backend::Symbol=:gl, kwargs...)
     _ensure_viz(backend=backend)
-    #return TopoViz.plot_persistence_entropy(args...; kwargs...)
     return Base.invokelatest(TopoViz.plot_persistence_entropy, args...; kwargs...)
 end
 function plot_betti_curvature(args...; backend::Symbol=:gl, kwargs...)
     _ensure_viz(backend=backend)
-    #return TopoViz.plot_betti_curvature(args...; kwargs...)
     return Base.invokelatest(TopoViz.plot_betti_curvature, args...; kwargs...)
 end
 function plot_betti_surface(args...; backend::Symbol=:gl, kwargs...)
     _ensure_viz(backend=backend)
-    #return TopoViz.plot_betti_curvature(args...; kwargs...)
     return Base.invokelatest(TopoViz.plot_betti_surface, args...; kwargs...)
 end
 function plot_data_set_overview(args...; backend::Symbol=:gl, kwargs...)
     _ensure_viz(backend=backend)
-    #return TopoViz.plot_betti_curvature(args...; kwargs...)
     return Base.invokelatest(TopoViz.plot_betti_curvature, args...; kwargs...)
 end
 
