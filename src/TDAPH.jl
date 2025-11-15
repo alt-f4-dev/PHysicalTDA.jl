c4693bca3833c8bae876c2e7804d1bb24c89f1c7
alt-f4-dev <isaac.ownby@gmail.com>
2025-11-15 14:56:49 -0500

    fixed super/sublevel logical flag

--- src/TDAPH.jl
+++ src/TDAPH.jl
@@ -19,10 +19,10 @@ function pd_array_intensity(A::AbstractArray{<:Real,N};
                 Z = zeros(size(A))
             else
                 A = (A .- amin) ./ (amax - amin)
-                Z = superlevel ? 1 .- A : A
+                Z = superlevel ? A : 1 .- A
             end
     else
-	    Z = superlevel ? 1 .- A : A
+	    Z = superlevel ? A : 1 .- A
     end
 	
     PD = ripserer(Cubical(Z); dim_max=maxdim)
